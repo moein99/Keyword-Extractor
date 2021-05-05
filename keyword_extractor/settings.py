59b7421f5515b05fd94d1666d2247b3714470616
moein <moein.mirzaei@cafebazaar.ir>
2021-05-05 19:15:00 +0430

    Improve template

--- keyword_extractor/settings.py
+++ keyword_extractor/settings.py
@@ -53,22 +53,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'keyword_extractor.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'keyword_extractor.wsgi.application'
 
 
